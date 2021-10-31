7fdb71ff60bdeb2e09d1324d01c7258a7ab1a2e6
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2021-10-31 19:56:03 +0200

    Update Untitled.m

--- Untitled.m
+++ Untitled.m
@@ -4,7 +4,7 @@ B=[16 3 2 13;5 10 11 8;9 6 7 12;4 15 14 1];
 C=[4 2 -3;7 -7 9;3 -5 6];
 D=[6 3 2;2 12 -7;-1 6 2;-5 15 11];
 %%
-%Calculate: 3A-5C, 7A+2B, CA, CD’
+%Calculate: 3A-5C, 7A+2B, CA, CDâ€™
 calc1=3*A-5*C;
 %calc2=7*a+2*b ERROR (because number of rows matrix A don't equal number of rows matrix B &
 % number of columns matrix A don't equal number of columns matrix B)
@@ -28,7 +28,7 @@ eye(n);
 %%
 %Try to find an easy way to build a 7*8-matrix whose other entries are
 %zeros, but in its diagonal and its last column are 5s
-z=diag([1 1 1 1 1 1 1]);z(:,8)=1;
+z=diag([5 5 5 5 5 5 5]);z(:,8)=5;
 %%
 %Output of:row with A(i,:) and column with A(:,j)
 i=2;j=1;
